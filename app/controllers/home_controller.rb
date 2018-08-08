2d3d0053e415731810daa9fd3e4e807a345deb89
tycompton <tyrone.compton@gmail.com>
2018-08-08 10:44:29 +0100

    Crypto lookup controller and views created

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,30 @@
 class HomeController < ApplicationController
   def index
+    require 'net/http'
+    require 'json'
+
+    @url = 'https://api.coinmarketcap.com/v1/ticker/'
+    @uri = URI(@url)
+    @response = Net::HTTP.get(@uri)
+    @coins = JSON.parse(@response)
+    @my_coins = ["BTC", "XRP", "ADA", "XLM", "STEEM"]
+
   end
 
-  #def about
-  #end
+  def about
+
+  end
+
+  def lookup
+    @symbol = params[:sym]
+
+    if @symbol 
+      @symbol = @symbol.upcase
+    end
+
+    if @symbol == ""
+      @symbol = "Please enter a currency"
+    end
+  end
 
 end
